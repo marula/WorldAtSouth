5a7630b0c55d6b70bfd08722d37ef10259182957
Rick Kleinhans <rick@baobablogic.com>
2013-03-29 08:57:01 +0200

    Update, contact, about, picture view, gig heights

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -1,14 +1,20 @@
 $(function() {
-  $(".thumb").click(function() {
+  $(".picture").click(function() {
     id = $(this).attr('id');
 
     $.ajax({
 		  url: window.location.pathname + "/" + id,
 		  context: document.body
 		}).done(function(data) {
-    	$(".normal").html('<img src="' + data.image + '">');
+    	$(".normal").html('<img src="' + data.image + '" class="pic_image round">');
     	$(".normal").fadeIn();
+    	$(".background").fadeIn();
 		});  
 
 	});
+
+	$(".background").click(function(){
+    $(".normal").fadeOut();
+    $(".background").fadeOut();
+  });   
 });
